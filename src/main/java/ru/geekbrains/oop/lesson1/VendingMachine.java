d9f855bad7e8e455744d82cfd055079048a78de4
«Ivan» <kolapstropa@gmail.com>
2024-03-18 04:11:55 +0200

    first commit

---
+++ src/main/java/ru/geekbrains/oop/lesson1/VendingMachine.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.oop.lesson1;
+
+import java.util.List;
+
+public class VendingMachine {
+
+    private List<Product> products;
+
+    public VendingMachine(List<Product> products){
+        this.products = products;
+    }
+
+
+    public BottleOfWater getBottleOfWater(String name, double volume){
+        for (Product product : products){
+            if (product instanceof BottleOfWater){
+                BottleOfWater bottleOfWater = (BottleOfWater)product;
+                if (bottleOfWater.getName().equals(name) && bottleOfWater.getVolume() == volume){
+                    return bottleOfWater;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public Chocolate getChocolate(String string, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getChocolate'");
+    }
+
+
+
+}
